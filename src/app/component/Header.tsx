003265f5934565539ff15bda0fb06edc1fd97469
David Shen <davidshen@DaviddeMacBook-Air.local>
2024-12-14 17:58:20 +0800

    second

--- src/app/component/Header.tsx
+++ src/app/component/Header.tsx
@@ -3,7 +3,7 @@ import { MailPlusIcon } from "lucide-react";
 const Header = () => {
     return (
         <div className="sticky top-0 p-5 flex justify-between items-start max-w-7xl mx-auto z-20 xl:items-center">
-            <div className="text-6xl">David&apos;s Blog</div>
+            <div className="text-6xl">David&apos;s Blog!!!!</div>
             <div>
                 <a href ="mailto:shenhantong@gmail.com">
                 <MailPlusIcon className="w-6 h-6"/>
